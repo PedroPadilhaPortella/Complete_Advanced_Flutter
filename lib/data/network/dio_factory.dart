0929498c374e6011f6c70c5cccfecc6ac0301ae4
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-11-18 00:40:47 -0300

    create dio factory

---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import '../../app/constant.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    int timeOut = 60 * 1000;
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constant.token,
+      DEFAULT_LANGUAGE: "en"
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: Constant.baseUrl,
+      connectTimeout: timeOut,
+      receiveTimeout: timeOut,
+      headers: headers,
+    );
+
+    if (kReleaseMode) {
+      print("release mode no logs");
+    } else {
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
+
+    return dio;
+  }
+}
